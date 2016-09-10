e46ab24699cb0b5c00f9e7f0552be22c912082b1
Sergey <sohohome.serg@gmail.com>
2016-09-10 17:33:08 +0300

    two my commit

---
+++ app/src/main/java/ru/sohohome/firstapp/AboutActivity.java
@@ -0,0 +1,13 @@
+package ru.sohohome.firstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_layout);
+    }
+}
--- app/src/main/java/ru/sohohome/firstapp/MainActivity.java
+++ app/src/main/java/ru/sohohome/firstapp/MainActivity.java
@@ -3,11 +3,41 @@ package ru.sohohome.firstapp;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
 public class MainActivity extends Activity {
+
+    private TextView label1;
+    private Button buttonTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        label1 = (TextView)findViewById(R.id.labelMain);
+        buttonTwo = (Button)findViewById(R.id.btTwo);
+
+        buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                label1.setText(R.string.msg_two);
+            }
+        });
+    }
+
+    public void btOneClick(View v){
+        /*
+        В layout об-ляем метод android:onClick="btOneClick"
+        */
+        label1.setText(R.string.msg_one);
+    }
+
+    public void btAboutClick(View v){
+        Intent i = new Intent(this, AboutActivity.class);
+        startActivity(i);
     }
 }
